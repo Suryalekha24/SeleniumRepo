5beb98ec42299055f8763ad051935ce6ae1b9daf
Suryalekha24 <suryalekha87@gmail.com>
2024-10-15 23:24:05 +0530

    Add files via upload
    week1-day1 files

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version= 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		System.out.println("Version of Chrome:"+version);
+		System.out.println("Developer of Chrome:"+developer);
+		System.out.println("Beta Status of chrome:"+isBeta);
+		System.out.println("Release year of chrome:"+releaseYear);
+		System.out.println("Shortcut key of Chrome:"+shortCutKey);
+
+	}
+
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		
+		int regNo=1234;
+		String colour="BLUE";
+		char logo='W';
+		boolean type=true;
+		float speed=0.45f;
+		double capacity= 12345.3344899;
+		long phNO=123456789;
+		short small=1234;
+		System.out.println("The colour of the car is  :"+colour);
+		System.out.println(type);
+		System.out.println(speed);
+	}
+}
